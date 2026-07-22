5ac55bbc01dbaa6d1f02c5184753884b82a6f63b
Bonie93 <thaobonie@gmail.com>
2026-07-22 10:59:38 +0700

    Add HSK1 vocabulary list with meanings and pinyin

--- data/hsk1.js
+++ data/hsk1.js
@@ -1,1 +1,28 @@
 const HSK1 = [];
+[
+  {
+    "hanzi": "你好",
+    "pinyin": "nǐ hǎo",
+    "meaning": "Xin chào"
+  },
+  {
+    "hanzi": "谢谢",
+    "pinyin": "xiè xie",
+    "meaning": "Cảm ơn"
+  },
+  {
+    "hanzi": "再见",
+    "pinyin": "zài jiàn",
+    "meaning": "Tạm biệt"
+  },
+  {
+    "hanzi": "是",
+    "pinyin": "shì",
+    "meaning": "Là"
+  },
+  {
+    "hanzi": "我",
+    "pinyin": "wǒ",
+    "meaning": "Tôi"
+  }
+]
